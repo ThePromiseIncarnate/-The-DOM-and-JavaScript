521163090dd77941dbce69dacd5581c7a23a1ff2
ThePromiseIncarnate <ThePromiseIncarnate@gmail.com>
2024-08-22 20:13:57 -0500

    Good enough for now

--- pets.js
+++ pets.js
@@ -1,7 +1,8 @@
 console.log("Hello from pets.js!");
-const pets = [];
-pets.push(createPet("Hi", 69, "Cat"));
-pets[0].status();
+
+const state = {
+    pets: [],
+}
 
 let savePetButton = document.getElementById("petButton");
 let petsNameInput = document.getElementById("petsName");
@@ -12,6 +13,8 @@ function savePet() {
     console.log(petsNameInput.value);
     console.log(petsAgeInput.value);
     console.log(petsSpeciesInput.value);
+    state.pets.push(createPet(petsNameInput.value, petsAgeInput.value, petsSpeciesInput.value));
+    console.table(state.pets);
 }
 
 savePetButton.addEventListener("click", savePet);
